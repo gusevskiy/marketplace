c95ffc04a8db24a42160c807ef8ca20f5788fb93
gusevskiy <vlgu@mail.ru>
2024-12-30 23:17:29 +0300

    подгоняю bootstrap

--- blade/catalog/views.py
+++ blade/catalog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from django.template import loader
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Главная страницы")
+    template = "blade/index.html"
+    return render(request, template)
 
 
 def catalog_list(request):
