e6202d09f22d386e7a7bb46aba6d46583a59a706
gusevskiy <vlgu@mail.ru>
2025-02-04 12:29:23 +0300

    выход вход работает

--- blade/users/urls.py
+++ blade/users/urls.py
@@ -1,5 +1,6 @@
 # Импортируем из приложения django.contrib.auth нужный view-класс
 from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView
+from django.views.generic import TemplateView
 from django.urls import path
 from . import views
 
@@ -11,21 +12,18 @@ urlpatterns = [
     # но префикс auth/ обрабатывется в головном urls.py
     path('signup/', views.SignUp.as_view(), name='signup'),
 
-    path(
-      'logout/',
-      # Прямо в описании обработчика укажем шаблон, 
-      # который должен применяться для отображения возвращаемой страницы.
-      # Да, во view-классах так можно! Как их не полюбить.
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
-    ),
-    
+
+
+    path('logout/', LogoutView.as_view(next_page='users:logged_out'), name='logout'),
+    path('logged_out/', TemplateView.as_view(template_name='users/logged_out.html'),
+         name='logged_out'),
+
     path(
         'login/',
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
-        # Восстановление пароля
+    # Восстановление пароля
     path(
         'password_reset/', PasswordResetView.as_view(
             template_name='users/password_reset_form.html'),
--- blade/users/views.py
+++ blade/users/views.py
@@ -1,6 +1,8 @@
 # users/views.py
 # Импортируем CreateView, чтобы создать ему наследника
+from ast import arg
 from django.views.generic import CreateView
+from django.contrib.auth.views import LogoutView
 
 # Функция reverse_lazy позволяет получить URL по параметрам функции path()
 # Берём, тоже пригодится
@@ -15,3 +17,9 @@ class SignUp(CreateView):
     # После успешной регистрации перенаправляем пользователя на главную.
     success_url = reverse_lazy('product:product_list')
     template_name = 'users/signup.html'
+
+
+# class CustomLogoutView(LogoutView):
+#     """ Для реализации выхода из аккаунта (тк в django 5 LogoutView не принимает)"""
+#     def post(self, request, *args, **kwargs):
+#         return self.get(request, *args, **kwargs)
