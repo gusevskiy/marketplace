1c90550ea420e50506aa202231916b63513153fc
gusevskiy <vlgu@mail.ru>
2025-02-20 00:04:01 +0300

    изменил форму добавления в корзину, убрал выпадающий список

--- blade/cart/forms.py
+++ blade/cart/forms.py
@@ -4,10 +4,12 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(
-        choices=PRODUCT_QUANTITY_CHOICES,
-        coerce=int,
-        label=""  # Это для того чтоб убрать слово "Quantity" из формы.
+    quantity = forms.IntegerField(
+        min_value=1,  # Минимальное значение
+        max_value=10,  # Максимальное значение (можно изменить)
+        initial=1,  # Значение по умолчанию
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 60px;'}),  # Виджет для ввода числа
+        label=""  # Убираем метку
     )
     override = forms.BooleanField(required=False,
                                   initial=False,
