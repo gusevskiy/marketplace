117bafc1ff55d8565a3aa8ef049388d23859f18f
gusevskiy <vlgu@mail.ru>
2025-03-05 21:05:48 +0300

    нужен кеш

--- cdek/views.py
+++ cdek/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 import requests
+import time
 from .cdek_api import CDEKAPI
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -14,8 +15,12 @@ def cities_and_pvz(request):
     if isinstance(cities, list) and cities and isinstance(cities[0], dict) and 'city' in cities[0]:
         cities.sort(key=lambda x: x['city'])
     
+    start1 = time.perf_counter()
     city_code = request.GET.get('city_code')
+    print(f"Запрос города занял {time.perf_counter() - start1:.2f} сек")
+    start2 = time.perf_counter()
     delivery_points = cdek_api.get_pvz_list(city_code) if city_code else []
+    print(f"Запрос ПВЗ занял {time.perf_counter() - start2:.2f} сек")
 
     # print(delivery_points)
 
